e6980232a81e1e9d5dfb38a4a0a338f797e9dbfe
David <masterdavidmoh@msn.com>
2016-03-30 17:28:02 +0200

    Created some classes

---
+++ Simulation Assignment/Event.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation_Assignment
+{
+    class Event: IComparable<Event>
+    {
+        public enum EventType {ArrivalPassenger, Queue, ArrivalTram, DepartureTram };
+
+        EventType eventType;
+        int time;
+
+        public Event(EventType eventType, int time)
+        {
+            this.eventType = eventType;
+            this.time = time;
+ 
+        }
+
+        public void executeEvent()
+        {
+
+
+        }
+
+        public int CompareTo(Event other)
+        {
+            if (this.time < other.time) return -1;
+            else if (this.time > other.time) return 1;
+            else return 0;
+        }
+    }
+}
