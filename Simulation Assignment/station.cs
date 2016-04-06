8a860dcf2e7c3af681befe5d7a126deced75a7b1
randoomed <doom.general@gmail.com>
2016-04-06 11:39:21 +0200

    added classes for trams and stations

---
+++ Simulation Assignment/station.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulation_Assignment
+{
+    class station
+    {
+        private List<int> arrivalTimes;
+        private int ID;
+        private string name;
+        private int nextStationID;
+
+        //maybe add data for inter arival times
+        //maybe add data for travel time to next station
+    }
+}
---
+++ Simulation Assignment/tram.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Simulation_Assignment
+{
+    class tram
+    {
+        private int ID;
+        private int passengers;
+        private int station; //id of the station its at or on its way to the queue for
+    }
+}
